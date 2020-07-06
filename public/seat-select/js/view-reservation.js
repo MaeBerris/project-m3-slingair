62955dd34e18d0cbdfa997180e2640e4baabb569
Mae <maeberris@outlook.com>
2020-07-06 09:52:44 -0400

    start working on reservation seat render

--- public/seat-select/js/view-reservation.js
+++ public/seat-select/js/view-reservation.js
@@ -1,9 +1,13 @@
 const emailDiv = document.querySelector("#email");
-function createSeats(data) {}
+const flightSelectMenu = document.querySelector(".flight-select");
+const flightSelect = document.querySelector("#flight");
+const reserationForm = document.querySelector("#form-content");
+flightSelectMenu.style.visibility = "hidden";
+let userEmail = "";
 
 const handleFormSubmit = async (event) => {
   event.preventDefault();
-  let userEmail = emailDiv.value;
+  userEmail = emailDiv.value;
   console.log(userEmail);
   let response = await fetch(`/reservation/${userEmail}`, {
     method: "GET",
@@ -13,4 +17,8 @@ const handleFormSubmit = async (event) => {
   });
   let data = await response.json();
   console.log(data);
+  let flights = [];
+  data.userArray.forEach((item) => {
+    flights.push(item.flight);
+  });
 };
--- test-data/reservations.js
+++ test-data/reservations.js
@@ -1,11 +1,27 @@
 const reservations = [
   {
     id: "88a33c23-3332-4ef2-bd71-be7a6430485f",
-    flight: "SA666",
-    seat: ["4D"],
+    flight: "SA123",
+    seat: ["1A"],
     givenName: "Marty",
     surName: "McFly",
     email: "marty@backfuture.com",
+  },
+  {
+    id: "eeac240a-89dd-40f2-b937-03ba1acb5501",
+    flight: "SA123",
+    seat: ["1C", "1D"],
+    givenName: "Mae",
+    surName: "Berris",
+    email: "maeberris@outlook.com",
+  },
+  {
+    id: "9bb27b4d-104c-4c85-a5de-7ca7167bd74b",
+    flight: "SA231",
+    seat: ["10F"],
+    givenName: "Mae",
+    surName: "Berris",
+    email: "maeberris@outlook.com",
   },
 ];
 
